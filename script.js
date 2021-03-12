a26c9aa15353eb681c6fb33f5d66e5f3ac69d4f7
jashvithlani <49621137+jashvithlani@users.noreply.github.com>
2021-03-12 17:33:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,103 +1,118 @@
+var fullobj,i;
+async function load(){
+    await fetch("./data.json")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        ccopy(data);
+        i=0;
+        changeToPhy();
+        qOnScreen(i);
+        createSideNav();
+        
+      });
+}
+    
+function ccopy(copOfData){
+    fullobj=copOfData;
+    
+}
+load();
+ 
+ 
 
-  var myObj, myJSON, text, obj;
-  
-  // Storing data:
-  obj =  [
+    var myObj, myJSON, text, obj;
+    
+    // fullobj=data;
+    //   const fullobj =  require('./data.json');
+      
+    
+    
+    function qOnScreen(k)
     {
-    ques:"this is question 1",
-    A:"option A q1",
-    B:"option B q1",
-    C:"option C q1",
-    D:"option D q1"
-    },
-
-    {
-      ques:"this is question 2",
-    A:"option A q2",
-    B:"option B q2",
-    C:"option C q2",
-    D:"option D q2"
-    },
-
-    {
-      ques:"this is question 3",
-    A:"option A q3",
-    B:"option B q3",
-    C:"option C q3",
-    D:"option D q3"
-    },
-
-    {
-      ques:"this is question 4",
-    A:"option A q4",
-    B:"option B q4",
-    C:"option C q4",
-    D:"option D q4"
-    },
-    {
-      ques:"this is question 5",
-    A:"option A q5",
-    B:"option B q5",
-    C:"option C q5",
-    D:"option D q5"
-    },
-    {
-      ques:"this is question 6",
-    A:"option A q6",
-    B:"option B q6",
-    C:"option C q6",
-    D:"option D q6"
-    },
-    {
-      ques:"this is question 7",
-    A:"option A q7",
-    B:"option B q7",
-    C:"option C q7",
-    D:"option D q7"
-    },
-    {
-      ques:"this is question 8",
-    A:"option A q8",
-    B:"option B q8",
-    C:"option C q8",
-    D:"option D q8"
+      var t=k+1;
+      document.getElementById("q_number").innerHTML="Q"+t
+      document.getElementById("question").innerHTML=obj[k].ques;
+      document.getElementById("option_a").innerHTML=obj[k].option_a;
+      document.getElementById("option_b").innerHTML=obj[k].option_b;
+      document.getElementById("option_c").innerHTML=obj[k].option_c;
+      document.getElementById("option_d").innerHTML=obj[k].option_a;
     }
-  
-  ] ;
-
-
-  function qonscreen(i)
-{
-  var t=i+1;
-  document.getElementById("q_number").innerHTML="Q"+t
-  document.getElementById("question").innerHTML=obj[i].ques;
-  document.getElementById("option_a").innerHTML=obj[i].A;
-  document.getElementById("option_b").innerHTML=obj[i].B;
-  document.getElementById("option_c").innerHTML=obj[i].C;
-  document.getElementById("option_d").innerHTML=obj[i].D;
+    function changeToMath(){
+        obj=fullobj.math;
+        i=0;
+    qOnScreen(i);
+    createSideNav();
+    }
+    function changeToPhyNum(){
+      obj=fullobj.phyNum;
+      i=0;
+  qOnScreen(i);
+  createSideNav();
+  }
+  function changeToChemNum(){
+    obj=fullobj.chemNum;
+    i=0;
+qOnScreen(i);
+createSideNav();
 }
-var i=0;
-qonscreen(i);
-
-function nextques()
-{
-  if(i<obj.length-1)
-  i++;
-  qonscreen(i);
-
-}
-
-
-function prevques()
-{
-  if(i>0)
-  i--;
-  qonscreen(i);
-}
-
-  // myJSON = JSON.stringify(myObj);
-  // localStorage.setItem("testJSON", myJSON);
-  
-  // // Retrieving data:
-  // text = localStorage.getItem("testJSON");
-  // obj = JSON.parse(text);
+    function changeToPhy(){
+        obj=fullobj.phy;
+        i=0;
+    qOnScreen(i);
+    createSideNav();
+    }
+    function changeToChem(){
+      obj=fullobj.chem;
+      i=0;
+  qOnScreen(i);
+  createSideNav();
+  }
+    
+    function nextQues()
+    {
+      if(i<obj.length-1)
+      i++;
+      qOnScreen(i);
+    
+    }
+    function createSideNav()
+    {
+        document.getElementById("sideNav").innerHTML="";
+        for(let j=0;j<obj.length;j++)
+        {
+            var newele= document.createElement("button");
+            newele.className="sideNavButton";
+            newele.innerHTML="Q"+obj[j].id;
+            newele.onclick=function(){
+            qOnScreen(j);
+            i=j;
+        }
+            document.getElementById("sideNav").appendChild(newele);
+        }
+    }
+    function navClick(temp1){
+        var temp=document.getElementsByClassName("navActive");
+        temp[0].className = temp[0].className.replace("navActive", "");
+        temp1.className = "navActive";
+    }
+    
+    function prevQues()
+    {
+      if(i>0)
+      i--;
+      qOnScreen(i);
+    }
+    
+    
+    // var i=0;
+    // qOnScreen(i);
+    // createSideNav();
+    
+      // myJSON = JSON.stringify(myObj);
+      // localStorage.setItem("testJSON", myJSON);
+      
+      // // Retrieving data:
+      // text = localStorage.getItem("testJSON");
+      // obj = JSON.parse(text);
